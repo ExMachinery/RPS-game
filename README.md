76de0eb167f83de43d2badbb6002b5ca0233dd2c
ExMachinery <milkcarrotc@gmail.com>
2023-05-19 01:03:01 +0600

    Add basic structure and starter comments

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RPS-game
+Simple Rock paper scissor game to accomplish something in JS programming.
+Not suppose to be a application right now, but in future it will.
+For now its weird console.log game.
---
+++ script.js
@@ -0,0 +1,41 @@
+//RPS-game
+
+//Use getComputerChoice function for random choice return. Use console log to make sure it works properly
+
+//Write a function for one round game with playerSelection and computerSelection parameters.
+//This function will return something like: "Haha you lose! Rock beats paper!"
+    //Make sure player selection is case-insensitive 
+    //Make sure your function return results, not console.log it
+
+//Write game() function. Use all functions inside this one. 5 games, keep scores and return a winner or looser based on 5 rounds on the end
+    //Try implement loop, tho you don't know how :D
+    //Use console.log to display results of each round and the winner at the end
+
+//Use prompt() to get input
+
+
+//Basic structure game()
+    //repeat this 5 times. Remember every round results. Define a winner.
+        //One round function. Compare two results. Somehow define who is a winner
+            //player choose on of the strings (if statement)
+            //return playerSelection
+            //-------
+            //getComputerChoice(): computer choose a random string (How?)
+            //return computerSelection
+
+
+
+
+
+
+
+
+
+
+/* 1. Understand exact problem. Refactor the problem, unless you could explain it in simple English to anyone.
+2. Plan a solving. 
+    - Does your program have a user interface? what will ot look like? What functionality will the interface have? Sketch it!
+    - What inputs will your program have? User input data or from somewhere else?
+    - What's the desired output?
+    - Given your inputs, what are the steps to return the desired output?
+3. Pseudocode. Write a program in a simple words. */
